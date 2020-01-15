bb1a7ed31dd9479477b007b7b9e9c6045e516a9e
renehorstmann <r.horsty@gmail.com>
2020-01-15 10:31:56 +0100

    header doc

--- include/plyc/header.h
+++ include/plyc/header.h
@@ -4,30 +4,29 @@
 extern "C" {
 #endif
 
-#include <stdlib.h>
-#include <stdbool.h>
-
 #include "types.h"
 
 
-
-/** Sets out_header_end to the start of the data (header end) */
+/**
+ * Sets out_header_end to the start of the data (header end).
+ * header_text must be a string, containing the ply header.
+ * If not found, an ply_err will be set as return value.
+ */
 ply_err ply_header_get_end(char **out_header_end, const char *header_text);
 
 
 /**
  * Parses the header, loaded from a ply file to memory.
  * If an error occurs (such that the given file is not a ply file),
- * an ply_err will be set as return value (see rcodes.h).
+ * an ply_err will be set as return value.
  */
 ply_err ply_header_parse(ply_File *out_header, const char *header_text);
 
 
-
 /**
- * Writes the header into an allocates string on the heap
+ * Writes the header into an allocated string on the heap.
  * If an error occurs,
- * an ply_err will be set as return value (see rcodes.h).
+ * an ply_err will be set as return value.
  */
 ply_err ply_header_write_to_heap(char **out_header_on_heap, ply_File header);
 
